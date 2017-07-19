4322e69dcd812cfeddb526da80d3469f7ba42336
의현 <ueehyeon@gmail.com>
2017-07-19 19:30:56 +0900

    상품등록페이지 DB 연결 완료 (but 시퀀스 요상함)

--- java/com/shoes/controller/addPRDTController.java
+++ java/com/shoes/controller/addPRDTController.java
@@ -8,11 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.shoes.dao.PRDTDAO;
+import com.shoes.dao.UsedTradeDAO;
 import com.shoes.model.CategoryBean;
 import com.shoes.model.PRDTBean;
 import com.shoes.model.PRDTCommonBean;
+import com.shoes.model.PrdtSizeTbBean;
 
 
 @Controller
@@ -20,6 +23,9 @@ public class addPRDTController{
 
 	@Autowired
 	private PRDTDAO prdtDao;
+	
+	@Autowired
+	private UsedTradeDAO usedDAO;
 	
 	@ModelAttribute   // ModelAttribute는 항상 RequestMapping된 메서드보다 먼저 실행된다. 메서드명은 자유롭게 지어도 되며, 보통 폼백킹이라고 지어준다.
 	   public PRDTCommonBean formBacking1(){   // 즉, 여기서 생성한 MemberVO를 디스패쳐서블릿 객체로 반환하고,
@@ -35,22 +41,31 @@ public class addPRDTController{
 	protected List<String> categoryData() {
 		
 		List<CategoryBean>beanList = new ArrayList<CategoryBean>();
-		beanList=prdtDao.getCategoryList();	
 		List<String> categoryList = new ArrayList<String>();
+		
+		beanList=prdtDao.getCategoryList();	
+		
 		for(int i=0; i<beanList.size(); i++){
-		   categoryList.add(beanList[i].getPRDT_CTG_NM());
+		   categoryList.add(beanList.get(i).getPRDT_CTG_NM());
 		}
 	return categoryList;
 	}
 	
-	
+
+
 	@ModelAttribute("PRDT_SIZE")
-	protected List<Integer> sizeData() throws Exception {
-	List<Integer> PRDT_SIZE = new ArrayList<Integer>();
-	for(int i=210; i<311; i+=5){
-	PRDT_SIZE.add(i);
-	}
-	return PRDT_SIZE;
+	protected List<String> sizeData() {
+		
+	List<PrdtSizeTbBean>beanList = new ArrayList<PrdtSizeTbBean>();
+	List<String> sizeList = new ArrayList<String>();
+	
+	beanList=prdtDao.getSizeList();
+	
+	for(int i=0; i<beanList.size(); i++){
+		   sizeList.add(beanList.get(i).getPRDT_SIZE_PK());
+		}
+	
+	return sizeList;
 	}
 	
 	@RequestMapping(value = "/admin", method = RequestMethod.GET)
@@ -68,21 +83,34 @@ public class addPRDTController{
 	}
 
 	@RequestMapping(value = "/addprdt", method = RequestMethod.GET)
-	public String addPRDT(){
-		return "admin/add_prdt";
+	public ModelAndView addPrdtGet(){
+		ModelAndView mav = new ModelAndView();
+
+		List<PrdtSizeTbBean> prdtSizeList = usedDAO.getPrdtSizeList();
+
+		mav.addObject("prdtSizeList", prdtSizeList);
+
+		mav.setViewName("admin/add_prdt");
+
+		System.out.println(prdtSizeList);
+		
+		return mav;
 	}
 	
-	
+
 	@RequestMapping(value="/addprdt", method=RequestMethod.POST)
 	public String submit(PRDTBean prdtBean){
      
 		System.out.println("넘어옴1");
 		prdtDao.insertPRDT(prdtBean);
 		System.out.println("넘어옴2");
-		return "redirect:/list";
+		return "redirect:/addprdtdone";
 	}
 
-	
+	@RequestMapping(value = "/addprdtdone", method = RequestMethod.GET)
+	public String addPRDTDone(){
+		return "admin/add_prdt_done";
+	}
 	
 	
 }
--- java/com/shoes/dao/PRDTDAO.java
+++ java/com/shoes/dao/PRDTDAO.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import com.shoes.model.CategoryBean;
 import com.shoes.model.PRDTBean;
 import com.shoes.model.PRDTCommonBean;
+import com.shoes.model.PrdtSizeTbBean;
 
 @Repository
 public class PRDTDAO {
@@ -25,13 +26,12 @@ public class PRDTDAO {
 	}
 
 	public List<CategoryBean> getCategoryList(){
-		List<CategoryBean> categoryList = new ArrayList<CategoryBean>();
-		categoryList = sqlSession.selectList("getCategoryList");
+		 List<CategoryBean> categoryList = sqlSession.selectList("getCategoryList");
 		return categoryList;
 	}
 	
-	public List<CategoryBean> getSizeList(){
-		List<CategoryBean> sizelist = sqlSession.selectList("getCategoryList");
+	public List<PrdtSizeTbBean> getSizeList(){
+		List<PrdtSizeTbBean> sizelist = sqlSession.selectList("getPrdtSizeList");
 		return sizelist;
 	}
 	
